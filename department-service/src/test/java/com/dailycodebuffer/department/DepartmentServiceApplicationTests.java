f45b7088316d493ef89d48c61f4ced8a159a2ed8
Jainil Jakasaniya <jainiljakasaniya@gmail.com>
2023-05-31 21:29:21 +0530

    add repo

---
+++ department-service/src/test/java/com/dailycodebuffer/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jainil.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/dailycodebuffer/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jainil.user.repository;
+
+import com.jainil.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
